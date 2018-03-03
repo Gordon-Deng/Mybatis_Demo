3a740514b6f3437730ec7dd4e6352ff6fb0d668d
Gordon <gordondeng@foxmail.com>
2018-03-03 19:22:32 +0800

    mybatis for session

---
+++ src/com/demomybatis/mapper/CategoryMapper.java
@@ -0,0 +1,30 @@
+package com.demomybatis.mapper;
+ 
+import java.util.List;
+
+import org.apache.ibatis.annotations.DeleteProvider;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.UpdateProvider;
+
+import com.demomybatis.CategoryDynaSqlProvider;
+import com.demomybatis.pojo.Category;
+ 
+public interface CategoryMapper {
+ 
+    @InsertProvider(type=CategoryDynaSqlProvider.class,method="add")  
+    public int add(Category category);  
+       
+  
+	@DeleteProvider(type=CategoryDynaSqlProvider.class,method="delete")
+    public void delete(int id);  
+       
+    @SelectProvider(type=CategoryDynaSqlProvider.class,method="get")  
+    public Category get(int id);  
+     
+    @UpdateProvider(type=CategoryDynaSqlProvider.class,method="update")  
+    public int update(Category category);   
+       
+    @SelectProvider(type=CategoryDynaSqlProvider.class,method="list")      
+    public List<Category> list();  
+}
--- src/com/demomybatis/pojo/Category.java
+++ src/com/demomybatis/pojo/Category.java
@@ -1,22 +1,19 @@
 package com.demomybatis.pojo;
 
 public class Category {
-    private int id;
-    private String name;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
